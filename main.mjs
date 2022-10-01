e98f5f5044f4c7be17f0b560b83a51d57b18f574
rassi0429 <taitokokoa+rassi@gmail.com>
2022-10-02 01:59:40 +0900

    完成

---
+++ main.mjs
@@ -0,0 +1,73 @@
+import { Client, GatewayIntentBits, REST, Routes } from "discord.js"
+import puppeteer from "puppeteer"
+import { v4 as uuidv4 } from 'uuid';
+import express from "express"
+const app = express()
+app.use("/internal", express.static('static'));
+app.use("/image", express.static('img'));
+
+app.listen(3000, () => console.log("express ok"))
+
+const commands = [
+    {
+        name: 'jellysay',
+        description: 'JELLYさんがしゃべります',
+        options: [
+            {
+                name: "text",
+                required: true,
+                description: "テキスト",
+                type: 3
+            }
+        ]
+    },
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+const setupCommand = async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+await setupCommand()
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on('guildCreate', async (guild) => {
+    guild.commands.set(commands).then(() =>
+        console.log(`Commands deployed in guild ${guild.name}!`));
+})
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'jellysay') {
+        const text = interaction.options.get("text")?.value
+        const  reply = await interaction.reply("generating...")
+        const uuid = await generateJELLYImage(text)
+        await interaction.channel.send(`https://${process.env.HOST}/image/${uuid}.png`)
+        await interaction.deleteReply()
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+const generateJELLYImage = async (text) => {
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'],  headless: true.valueOf });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1280, height: 720 });
+    await page.goto("http://localhost:3000/internal/index.html?text=" + text);
+    await page.waitForTimeout(500);
+    const uuid = uuidv4()
+    await page.screenshot({ path: `./img/${uuid}.png` });
+    await browser.close();
+    return uuid
+}
